1f2aa9626c8acc9a5fd26a8a1032f83a1870a317
ANZAR <anzarkhan790@gmail.com>
2025-02-23 10:01:27 +0530

    aws configs

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,8 +26,8 @@ app.use('/users',userRoutes)
 app.use('/expenses',expenseRoutes)
 
 
-const port=process.env.PORT || 3001;
+const port=Number(process.env.PORT) || 3001;
 
-app.listen(port,()=>{
+app.listen(port,"0.0.0.0",()=>{
     console.log(`Server is running on port ${port}`);
 })
